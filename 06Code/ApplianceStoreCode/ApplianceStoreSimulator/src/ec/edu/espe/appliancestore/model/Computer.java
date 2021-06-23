c1ebbc2bac28350fd071993174184fc502772cd4
SalazarMatthew <emsalazar1@espe.edu.ec>
2021-06-23 18:39:54 -0500

    ObjetComputer

--- 06Code/ApplianceStoreCode/ApplianceStoreSimulator/src/ec/edu/espe/appliancestore/model/Computer.java
+++ 06Code/ApplianceStoreCode/ApplianceStoreSimulator/src/ec/edu/espe/appliancestore/model/Computer.java
@@ -14,7 +14,37 @@ public class Computer {
     private String material;
     private float price;
     private String storage;
-    private int serialnumber;
+    private int serialNumber;
+    
+    public Computer(){
+        this.power = 0;
+        this.material = "";
+        this.price = 0.0F;
+        this.storage = "";
+        this.serialNumber = 0;
+    }
+    
+    public Computer(int power,String material,float price,String storange,int serialNumber){
+        this.power = power;
+        this.material = material;
+        this.price = price;
+        this.storage = storange;
+        this.serialNumber = serialNumber;
+    }
+    
+    public Computer(String power, String material, String price, String storange, String serialNumber){
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public String toString() {
+        return "Computer{" + "Power -> " + power + " Material -> " + material + " Price -> " + price + " Storage ->" + storage + ", SerialNumber -> " + serialNumber + '}';
+    }
+    
+    public String [] getArray(){
+        String[] date = {"Power -> " + String.valueOf(power)+ " Material ->" +String.valueOf(material)+ " Price -> "+String.valueOf(price)+" Storage -> "+String.valueOf(storage)+ " SerialNumber -> "+String.valueOf(serialNumber)};
+        return date;
+    }
         
     public void buy(){
         
@@ -29,4 +59,76 @@ public class Computer {
     return new Gain();
     
     }
+
+    /**
+     * @return the power
+     */
+    public int getPower() {
+        return power;
+    }
+
+    /**
+     * @param power the power to set
+     */
+    public void setPower(int power) {
+        this.power = power;
+    }
+
+    /**
+     * @return the material
+     */
+    public String getMaterial() {
+        return material;
+    }
+
+    /**
+     * @param material the material to set
+     */
+    public void setMaterial(String material) {
+        this.material = material;
+    }
+
+    /**
+     * @return the price
+     */
+    public float getPrice() {
+        return price;
+    }
+
+    /**
+     * @param price the price to set
+     */
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    /**
+     * @return the storage
+     */
+    public String getStorage() {
+        return storage;
+    }
+
+    /**
+     * @param storage the storage to set
+     */
+    public void setStorage(String storage) {
+        this.storage = storage;
+    }
+
+    /**
+     * @return the serialNumber
+     */
+    public int getSerialNumber() {
+        return serialNumber;
+    }
+
+    /**
+     * @param serialNumber the serialNumber to set
+     */
+    public void setSerialNumber(int serialNumber) {
+        this.serialNumber = serialNumber;
+    }
+    
+    
 }
